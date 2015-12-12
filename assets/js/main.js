46cedeb48a164a3da9481c28a6d8f2f8eb8722bf
stephanietc <stephanietc@users.noreply.github.com>
2015-12-12 13:20:50 -0500

    update feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,8 +52,4 @@ jQuery(document).ready(function($) {
         }
     );
 
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
 });
